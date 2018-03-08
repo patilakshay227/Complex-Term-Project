243a7be4a9efe2108a2f9ecf4768ab58cdda9fc3
ashwinbhy <ashwinbhy@gmail.com>
2018-03-09 00:53:21 +0530

    mod fieldcheck

--- fieldCheck.py
+++ fieldCheck.py
@@ -1,5 +1,37 @@
-# -*- coding: utf-8 -*-
-import sqlite3
-import datetime
+import simplejson as json
+import pickle
 
-from bs4 import BeautifulSoup
+def main():
+    skip=1
+    done=1
+    good = open("good", "a")
+    with open("dailyComments") as f:
+        for line in f:
+            try:
+                line = line.split("\t")[2]
+                value=json.loads(line.rstrip())
+                val=json.dumps(value)
+                good.write(val+"\n")
+                done += 1
+            except:
+                skip+=1
+    good.close()
+    print "done ",done
+
+myd=dict()
+def readids():
+    with open("/media/ashwin/E0C62B17C62AED8A/Study material/IIT Kgp/Sem2/Comple/Project/good") as f:
+        for line in f:
+            line=json.loads(line)            
+            for l in line['results']['comments']:
+                print l.keys()
+                break
+                id=l['assetID']
+                if l['commentTitle']!='<br/>':
+                    print l['commentTitle']
+            break
+    for key in myd.keys():
+        print key,' : ', myd[key]
+
+if __name__ == "__main__":
+	readids()
